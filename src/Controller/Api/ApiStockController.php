6b8836029ff9a0ca0deb872a8dd2a1a2ebcbc740
jihanejador <jihanejador@gmail.com>
2026-06-18 17:44:14 +0100

    creation un class de apistockcontroller

--- src/Controller/Api/ApiStockController.php
+++ src/Controller/Api/ApiStockController.php
@@ -4,3 +4,9 @@ namespace App\Controller\Api;
 use App\Service\AuthService;
 use App\Service\StockService;
 
+class ApiStockController{
+    private AuthService $authService;
+    private StockService $stockService;
+
+    
+}
