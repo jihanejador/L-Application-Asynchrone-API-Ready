df92b0c7c2b14155ace64a5e67b6f240c76bb1bf
jihanejador <jihanejador@gmail.com>
2026-06-18 16:17:41 +0100

    faire un condition

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -3,6 +3,8 @@ namespace App\Service;
 
 class AuthService{
     public function __construct(){
-        
+        if(session_status() === PHP_SESSION_NONE){
+            session_start();
+        }
     }
 }
