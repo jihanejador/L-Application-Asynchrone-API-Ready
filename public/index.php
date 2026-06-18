1803f97240b1d78c0698f92a657bb767150e3daa
jihanejador <jihanejador@gmail.com>
2026-06-18 15:56:58 +0100

    declare les variables

--- public/index.php
+++ public/index.php
@@ -5,3 +5,8 @@ use App\Controller\Web\AuthController;
 use App\Controller\Web\AdminController;
 use App\Controller\Api\ApiStockController;
 use App\Controller\Api\ApiDshboardController;
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+
