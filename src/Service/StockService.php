b9a89673cea03cf9dc3d6b5216f89bf9df245604
jihanejador <jihanejador@gmail.com>
2026-06-18 16:27:53 +0100

    creation la class de stockservice

--- src/Service/StockService.php
+++ src/Service/StockService.php
@@ -3,3 +3,11 @@ namespace App\Service;
 
 use App\Config\Database;
 use PDO;
+
+class StockService{
+    private PDO $db;
+
+    public function __construct(){
+        $this->db = Database::getConnection();
+    }
+}
