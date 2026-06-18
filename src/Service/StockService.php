41fc74579aede6f114da482b69cb255c57df5524
jihanejador <jihanejador@gmail.com>
2026-06-18 16:53:11 +0100

    inserer un nouveau lot

--- src/Service/StockService.php
+++ src/Service/StockService.php
@@ -10,4 +10,17 @@ class StockService{
     public function __construct(){
         $this->db = Database::getConnection();
     }
+
+    public function insertBatch(int $medicamentId, int $quantite, string $datePeremption, string $numLot): bool {
+        $stmt = $this->db->prepare("
+        INSERT INTO batches (medicament_id, quantity, date_peremption, num_lot, status)
+        VALUES (:med_id, :qty, :date_p, :num_l, 'AVAILABLE')
+        ");
+        return $stmt->execute([
+            'med_id' => $medicamentId,
+            'qty' => $quantite,
+            'date_p' => $datePeremption,
+            'num_l' => $numLot
+        ]);
+    }
 }
