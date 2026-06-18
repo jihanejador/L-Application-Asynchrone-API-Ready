0d9755571c2871d01b888a683ce79ee68500832b
jihanejador <jihanejador@gmail.com>
2026-06-18 18:36:31 +0100

    creation un class de api dashboard

--- src/Controller/Api/ApiDashboardController.php
+++ src/Controller/Api/ApiDashboardController.php
@@ -4,3 +4,8 @@ namespace App\Controller\Api;
 use App\Service\AuthService;
 use App\Config\Database;
 use PDO;
+
+class ApiDashboardController{
+    private AuthService $authService;
+    private PDO $db;
+}
