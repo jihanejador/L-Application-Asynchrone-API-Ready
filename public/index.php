394da518bb838033e24552ac56af84c3078c2b80
jihanejador <jihanejador@gmail.com>
2026-06-19 11:57:50 +0100

    changer le vendor/autoload.php en code clair

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
+spl_autoload_register(function ($class) {
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) return;
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) { require_once $file; }
+});
 use App\Controller\Web\AuthController;
 use App\Controller\Web\AdminController;
 use App\Controller\Api\ApiStockController;
